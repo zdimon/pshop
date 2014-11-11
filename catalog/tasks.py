e82c95fb5dce09bd3a40f18f3f9a7e47be1f2304
zarik <zdimon77@gmail.com>
2014-11-11 18:14:00 +0200

    dd

--- catalog/tasks.py
+++ catalog/tasks.py
@@ -17,8 +17,8 @@ def reg(user):
     url = REGISTRATION_URL
     sign = hashlib.md5(SECRET+user.username).hexdigest()
     data = {'username': user.username, 'email': user.email, 'mirror_id': MIRROR_ID, 'sign': sign}
-    import pdb; pdb.set_trace()
-    logger.info(data)
+    #import pdb; pdb.set_trace()
+    logger.info('request %s' % (url,))
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     logger.info(url)
     r = requests.post(url, data=json.dumps(data), headers=headers)
