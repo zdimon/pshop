620789c485d154322ac31913d5f2067d007d8e61
zarik <zdimon77@gmail.com>
2014-12-29 12:51:36 +0200

    auto commit

--- catalog/templatetags/new_pressa_tag.py
+++ catalog/templatetags/new_pressa_tag.py
@@ -5,7 +5,7 @@ register = template.Library()
 @register.inclusion_tag("catalog/in_tags.html")
 def new_pressa_tag():
     out = {}
-    c = Journal.objects.all().order_by(in_popular_pressa=True)
+    c = Journal.objects.filter(in_popular_pressa=True).all()
     out['items'] = c
     out['class'] = 'new_pressa_tag'
     return out
